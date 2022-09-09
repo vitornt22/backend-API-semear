92ff60f11b2596fb10b994de94c17863554fb654
Vitor Neto <vitornt434@gmail.com>
2022-09-09 16:41:26 -0300

    add changes to deploy in heroku

--- semear/settings.py
+++ semear/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+import django_on_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,3 +153,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())

cd303e2018815497a321219dfba60b6b04beb14e
Vitor Neto <vitornt434@gmail.com>
2022-09-17 20:58:46 -0300

    changes

--- church/views.py
+++ church/views.py
@@ -1,5 +1,8 @@
 
 
+from informations.models import PIX, Adress, BankData
+from informations.serializers import (AdressSerializer, BankDataSerializer,
+                                      PIXSerializer)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from user.models import UserModel
@@ -23,8 +26,39 @@ class ChurchApi (ModelViewSet):
             post_data["user.password"]
         )
         new_user.save()
+
+        adress = Adress.objects.create(
+            adress=post_data["adress.adress"],
+            zip_code=post_data["adress.zip_code"],
+            number=post_data["adress.number"],
+            city=post_data["adress.city"],
+            uf=post_data['adress.uf'],
+            district=post_data['adress.district']
+        )
+        adress.save()
+
+        bankData = BankData.objects.create(
+            holder=post_data["bankData.holder"],
+            cnpj=post_data["bankData.cnpj"],
+            bank=post_data["bankData.bank"],
+            agency=post_data["bankData.agency"],
+            digitAgency=post_data['bankData.digitAgency'],
+            account=post_data['bankData.account'],
+            digitAccount=post_data['bankData.digitAccount'],
+        )
+        bankData.save()
+
+        pix = PIX.objects.create(
+            typeKey=post_data['pix.typeKey'],
+            valueKey=post_data['pix.valueKey']
+        )
+        pix.save()
+
         new_church = Church.objects.create(
             user=new_user,
+            adress=adress,
+            bankData=bankData,
+            pix=pix,
             cnpj=post_data['cnpj'],
             ministery=post_data['ministery'],
             name=post_data['name']

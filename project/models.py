80e6e1aa89aa71675975d0e979b85676da4aa8cf
Vitor Neto <vitornt434@gmail.com>
2022-09-20 20:40:18 -0300

    changes

--- project/models.py
+++ project/models.py
@@ -1,6 +1,7 @@
 from church.models import Church
 from django.db import models
 from informations.models import Adress
+from user.models import UserModel
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Project (models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     id_church = models.IntegerField()
     id_adress = models.IntegerField(null=False)
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
     church = models.ForeignKey(
         Church, on_delete=models.CASCADE, null=True, blank=True)
     adress = models.ForeignKey(Adress, on_delete=models.CASCADE)

faef9f7fe654824a64e00f2a0e6a621727a8118d
Vitor Neto <vitornt434@gmail.com>
2022-09-20 12:18:39 -0300

    changes

--- missionary/models.py
+++ missionary/models.py
@@ -7,6 +7,7 @@ from user.models import UserModel
 # Create your models here.
 class Missionary(models.Model):
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
+    id_church = models.IntegerField()
     church = models.ForeignKey(
         Church, on_delete=models.CASCADE, null=True, blank=True)
     fullName = models.CharField(max_length=80, null=False, blank=False)

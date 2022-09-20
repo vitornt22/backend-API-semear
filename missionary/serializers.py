ee65dc1c53c8d27928474b539ce40b6e6da4fb07
Vitor Neto <vitornt434@gmail.com>
2022-09-20 16:39:05 -0300

    changes

--- missionary/serializers.py
+++ missionary/serializers.py
@@ -1,3 +1,4 @@
+from church.serializers import ChurchSerializer
 from informations.serializers import AdressSerializer
 from rest_framework import serializers
 from user.serializers import UserSerializer
@@ -8,6 +9,7 @@ from .models import Missionary
 class MissionarySerializer(serializers.ModelSerializer):
     user = UserSerializer()
     adress = AdressSerializer(allow_null=True, required=False)
+    church = ChurchSerializer(allow_null=True, required=False)
 
     class Meta:
         model = Missionary

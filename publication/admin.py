6482acf30da0ac54b035d8a6f8f219e922519b8d
Vitor Neto <vitornt434@gmail.com>
2022-09-27 19:14:49 -0300

    changes

--- publication/admin.py
+++ publication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Publication
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    ...
--- publication/views.py
+++ publication/views.py
@@ -13,5 +13,22 @@ class PublicationApi (ModelViewSet):
     serializer_class = PublicationSerializer
 
     def create(self, request, *args, **kwargs):
+        post_data = request.data
+        print("REQUEST PUBLICATION", request.data)
 
-        return Response({})
+        try:
+            a = UserModel.objects.get(id=post_data['id'])
+        except UserModel.DoesNotExist:
+            a = UserModel.objects.get(id=1)
+
+        new_Publication = Publication.objects.create(
+            user=a,
+            upload=request.FILES['upload'],
+            legend=post_data['legend'],
+            is_accountability=False
+        )
+
+        new_Publication.save()
+        serializer = PublicationSerializer(new_Publication)
+
+        return Response(serializer.data)

777b071615554f3931d10a58fe51f65d176bc77b
Vitor Neto <vitornt434@gmail.com>
2022-10-08 16:55:53 -0300

    changes

--- publication/serializers.py
+++ publication/serializers.py
@@ -1,5 +1,9 @@
 
 
+from missionary.models import Missionary
+from missionary.serializers import MissionarySerializer
+from project.models import Project
+from project.serializers import ProjectSerializer
 from rest_framework import serializers
 from user.serializers import UserSerializer
 
@@ -22,6 +26,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class PublicationSerializer(serializers.ModelSerializer):
     user = UserSerializer(allow_null=True)
+    project = serializers.SerializerMethodField(
+        'get_project')
+    missionary = serializers.SerializerMethodField(
+        'get_missionary')
     likes = serializers.SerializerMethodField(
         'get_likes')
     comments = serializers.SerializerMethodField(
@@ -30,6 +38,22 @@ class PublicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publication
         fields = '__all__'
+
+    def get_project(self, obj):
+        id = obj.user.id
+        if obj.user.category == 'project':
+            result = Project.objects.get(user=id)
+            return ProjectSerializer(result).data
+        else:
+            return None
+
+    def get_missionary(self, obj):
+        id = obj.user.id
+        if obj.user.category == 'missionary':
+            result = Missionary.objects.get(user=id)
+            return MissionarySerializer(result).data
+        else:
+            return None
 
     def get_likes(self, obj):
         id = obj.id

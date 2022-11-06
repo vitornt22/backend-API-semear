2323f5a03adfe2fcac708e1255d7c95e440dd5a1
Vitor Neto <vitornt434@gmail.com>
2022-11-06 16:12:56 -0300

    changes

--- transaction/models.py
+++ transaction/models.py
@@ -2,15 +2,16 @@
 from email.policy import default
 
 from django.db import models
+
 from user.models import UserModel
 
 
 # Create your models here.
 class Donation(models.Model):
     user = models.ForeignKey(
-        UserModel, related_name='user', on_delete=models.CASCADE, null=True)
+        UserModel, related_name='user', on_delete=models.SET_NULL, null=True)
     donor = models.ForeignKey(
-        UserModel, related_name='donor_field', on_delete=models.CASCADE, null=True)  # noqa
+        UserModel, related_name='donor_field', on_delete=models.SET_NULL, null=True)  # noqa
     is_anonymous = models.BooleanField(default=False)
     value = models.FloatField()
     payment_form = models.CharField(max_length=50)

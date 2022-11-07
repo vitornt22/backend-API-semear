d346d58143eb1b73de8d13e36be480083e427256
Vitor Neto <vitornt434@gmail.com>
2022-11-06 21:27:28 -0300

    changes

--- user/models.py
+++ user/models.py
@@ -38,7 +38,7 @@ class UserManager(BaseUserManager):
 
         user.is_admin = True
         user.is_staff = True
-        #user.is_active = True
+        user.is_active = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
